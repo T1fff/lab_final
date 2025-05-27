bd16f9b5d0e9c6d4095d315d96234d29a3405bce
carrotlover25 <fang@uninorte.edu.co>
2025-05-27 18:54:23 -0500

    implementacion interfaz

--- interfaz.py
+++ interfaz.py
@@ -2,11 +2,13 @@
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 from red import RedEnergia
+from PIL import Image, ImageTk
 
 class AplicacionRed:
     def __init__(self, root):
         self.root = root
         self.root.title("Red de Energía Renovable")
+        self.root.geometry("1152x648")
         self.red = RedEnergia()
         self.canvas_frame = None
         self.ruta_nodos = tk.StringVar()
@@ -14,39 +16,110 @@ class AplicacionRed:
         self.nodo_inicio = tk.StringVar()
         self.nodo_fin = tk.StringVar()
         self.estrategia = tk.StringVar(value="sostenibilidad")
-        self._crear_interfaz()
+        
+        self.frames = {}
+        self.crear_frames()
+        self.estilo_boton_ttk()
+        self.mostrar_frames("inicio")
+        
+    def crear_frames(self):
+        frame_inicio = self.crear_frame_inicio()
+        frame_redFlujo = self.crear_frame_red()
+        
+        self.frames["inicio"] = frame_inicio
+        self.frames["Red Flujo"] = frame_redFlujo
+        
+        for frame in self.frames.values():
+            frame.place(relwidth=1, relheight=1)
+            
+    def mostrar_frames(self, nombre_frame):
+        frame = self.frames[nombre_frame]
+        frame.tkraise()
+        
+    def estilo_boton_ttk(self):
+        style = ttk.Style()
 
-    def _crear_interfaz(self):
-        frame = ttk.Frame(self.root, padding=10)
-        frame.pack(fill="both", expand=True)
-
-        ttk.Label(frame, text="Archivo de Nodos CSV").pack()
-        ttk.Entry(frame, textvariable=self.ruta_nodos, width=50).pack()
-        ttk.Button(frame, text="Seleccionar", command=self._cargar_nodos).pack()
-
-        ttk.Label(frame, text="Archivo de Conexiones CSV").pack()
-        ttk.Entry(frame, textvariable=self.ruta_conexiones, width=50).pack()
-        ttk.Button(frame, text="Seleccionar", command=self._cargar_conexiones).pack()
-
-        ttk.Button(frame, text="Cargar Red", command=self._cargar_red).pack(pady=10)
-
-        ttk.Label(frame, text="Nodo Inicio").pack()
+        style.configure("estilo.TButton",
+                        font=("Segoe Ui", 10, "bold"),
+                        padding = 10,
+                        background="#007ACC",
+                        foreground="black")
+        style.map("estilo.TButton",
+                   background=[
+            ("active", "#005F9E"),
+            ("pressed", "#004C80")
+        ],
+        foreground=[
+            ("pressed", "black"),
+            ("active", "black")
+        ])
+        
+    def crear_frame_inicio(self):
+        frame = tk.Frame(self.root)
+        
+        # fondo
+        image1 = Image.open("assets/image1_inicio.png")
+        fondo = ImageTk.PhotoImage(image1)
+        fondo_label = tk.Label(frame, image=fondo)
+        fondo_label.image = fondo  # mantener referencia
+        fondo_label.place(relwidth=1, relheight=1)
+        
+        boton_start = tk.Button(frame, 
+                                text="Start", 
+                                font = ("Arial", 18, "bold"),
+                                width = 15,
+                                height = 2,
+                                bg="#f1fbff",               
+                                fg="black",                 # Color de texto normal
+                                activebackground="#e3f7ff", # Color al presionar
+                                activeforeground="black",   # Texto al presionar
+                                relief="raised",            # Relieve inicial
+                                bd=5,  
+                                command=lambda: self.mostrar_frames("Red Flujo"))
+        boton_start.place(relx=0.5, rely=0.7, anchor="center")
+        
+        return frame
+    
+    def crear_frame_red(self):
+        frame = tk.Frame(self.root)
+        
+        image2 = Image.open("assets/image2_red.png")
+        fondo = ImageTk.PhotoImage(image2)
+        fondo_label = tk.Label(frame, image=fondo)
+        fondo_label.image = fondo  # mantener referencia
+        fondo_label.place(relwidth=1, relheight=1)
+        
+        # seleccionar archivo de nodos
+        ttk.Entry(frame, textvariable=self.ruta_nodos, width=50).place(x=90, y=200)
+        ttk.Button(frame, text="Seleccionar", style="estilo.TButton", command=self._cargar_nodos).place(x=190, y=230)
+        
+        # seleccionar archivo de conexiones
+        ttk.Entry(frame, textvariable=self.ruta_conexiones, width=50).place(x=700, y=70)
+        ttk.Button(frame, text="Seleccionar", style="estilo.TButton" ,command=self._cargar_conexiones).place(x=800, y=100)
+        
+        # botón de cargar ruta (tengo q ver como rayos hago esto)
+        ttk.Button(frame, text="Cargar Red", style="estilo.TButton",command=self._cargar_red).place(x=800, y=200)
+        
+        # seleccionar nodo inicio
         self.combo_inicio = ttk.Combobox(frame, textvariable=self.nodo_inicio)
-        self.combo_inicio.pack()
-
-        ttk.Label(frame, text="Nodo Fin").pack()
+        self.combo_inicio.place(x=120, y=330, width= 200)
+        
+        # seleccionar nodo final
         self.combo_fin = ttk.Combobox(frame, textvariable=self.nodo_fin)
-        self.combo_fin.pack()
+        self.combo_fin.place(x=735, y=330, width=200)
+        
+        # escoger estrategia
+        pos_y = 450
+        pos_x = 20
+        for val in ["1", "2", "3"]:
+            ttk.Radiobutton(frame, text=val.capitalize(), value=val, variable=self.estrategia).place(x=pos_x, y=pos_y)
+            pos_y += 40
 
-        ttk.Label(frame, text="Estrategia").pack()
-        for val in ["sostenibilidad", "perdida", "produccion"]:
-            ttk.Radiobutton(frame, text=val.capitalize(), value=val, variable=self.estrategia).pack(anchor="w")
-
-        ttk.Button(frame, text="Calcular Ruta Óptima", command=self._calcular_ruta).pack(pady=10)
-
-        self.canvas_frame = tk.Frame(self.root)
-        self.canvas_frame.pack(fill="both", expand=True)
-
+        # calcular ruta óptima
+        ttk.Button(frame, text="Calcular Ruta Óptima", style="estilo.TButton", command=self._calcular_ruta).place(x=750, y=400)
+        
+        return frame
+    
     def _cargar_nodos(self):
         archivo = filedialog.askopenfilename(filetypes=[("CSV", "*.csv")])
         if archivo:
@@ -81,11 +154,30 @@ class AplicacionRed:
         ruta, costo = self.red.encontrar_ruta_optima(inicio, fin, estrategia)
         if ruta:
             messagebox.showinfo("Ruta Óptima", f"Ruta: {' → '.join(ruta)}\nCosto: {costo:.2f}")
-            self._visualizar(ruta, estrategia)
+            self._mostrar_popup_grafo(ruta, estrategia)
         else:
             messagebox.showwarning("Sin Ruta", "No se encontró una ruta válida.")
 
     def _visualizar(self, ruta=None, estrategia=None):
-        for widget in self.canvas_frame.winfo_children():
-            widget.destroy()
+        if self.canvas_frame:
+            self.canvas_frame.destroy()
+
+        self.canvas_frame = tk.Toplevel(self.root)
+        self.canvas_frame.title("Visualización de Red")
+        self.canvas_frame.geometry("900x600")
+
         self.red.visualizar_red(self.canvas_frame, ruta, estrategia)
+    
+    
+    
+    def _mostrar_popup_grafo(self, ruta, estrategia):
+        ventana = tk.Toplevel(self.root)
+        ventana.title("Visualización de Ruta Óptima")
+        ventana.geometry("800x600")
+
+        # Crear frame para el canvas
+        frame_canvas = tk.Frame(ventana)
+        frame_canvas.pack(fill="both", expand=True)
+
+        # Visualizar red en ese frame
+        self.red.visualizar_red(frame_canvas, ruta, estrategia)
